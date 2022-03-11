c7f784a92ddc8f1adef54f52ce9e952187b586cf
Matthew Malvini <matthew.malvini@sjsu.edu>
2022-03-10 22:18:51 -0800

    document_analyzer has been edited

--- document_analyzer.py
+++ document_analyzer.py
@@ -1,10 +1,15 @@
 def counter():
-	with  open('documents.txt') as f:
+	with  open('document.txt') as f:
 		contents = f.read()
 		print(contents)	
-
+	contents.split()
+	splitting=contents.split()
+	print(splitting)
+	i=0
 	while i<5:
 		print("word: ")
-	i=i+1
-	return contents
+		i=i+1
 	f.close()
+	return contents
+
+counter()
